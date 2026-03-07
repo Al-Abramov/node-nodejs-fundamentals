ddde285a286ba1f56ac55d2c9dc5de5a758d85b7
Абрамов Алексей <x3m.f@mail.ru>
2026-03-07 23:37:10 +0400

    fix progress.js tasks

--- src/cli/progress.js
+++ src/cli/progress.js
@@ -48,7 +48,9 @@ const progress = () => {
     let filledBar = CHAR.repeat(filled);
     const emptyBar = ' '.repeat(empty);
 
-    if (config['--color'] && /^#([0-9a-fA-F]{6})$/.test(config['--color'])) {
+    const isColorValid = /^#([0-9a-fA-F]{6})$/.test(config['--color']);
+
+    if (config['--color'] && isColorValid) {
       const r = parseInt(config['--color'].slice(1, 3), 16);
       const g = parseInt(config['--color'].slice(3, 5), 16);
       const b = parseInt(config['--color'].slice(5, 7), 16);
@@ -58,11 +60,13 @@ const progress = () => {
       filledBar = `\x1b[38;2;${rgb}m${filledBar}\x1b[0m`;
     }
 
+    process.stdout.write('\x1B[?25l');
     process.stdout.write(`[${filledBar}${emptyBar}] ${Math.round(progress)}%\r`);
 
     if (progress >= 100) {
       clearInterval(interval);
       console.log('\nDone!');
+      process.stdout.write('\x1B[?25h');
     }
   }, config['--interval']);
 
